0f742f3f4a21727a94f63c7c2f2485929ccbc25e
Víctor Córdova <theslapboy@gmail.com>
2022-02-16 20:49:19 -0800

    Adding the home title

---
+++ lib/models/global.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+const Color darkGreyColor = Color(0xFF212128);
+const Color lightBlueColor = Color(0xFF8787A0);
+const Color redColor = Color(0xFFDC4F64);
+
+TextStyle intrayTitleStyle = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 50);
+
+TextStyle bigLightBlueTitle = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: lightBlueColor,
+    fontSize: 50);
+
+TextStyle darkTodoTitle = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 30);
+
+TextStyle whiteTitle = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontSize: 20);
+
+TextStyle whiteButtonTitle = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontSize: 15);
+TextStyle redTodoTitle = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: redColor,
+    fontSize: 30);
+
+TextStyle redBoldText = const TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: redColor,
+    fontSize: 20);
+
+TextStyle redText =
+    const TextStyle(fontFamily: 'Avenir', color: redColor, fontSize: 20);
